33cb885dc2dd4ecb0c70e080cbd4fead55cfd3d5
Alzheimer720 <7anantsharma7@gmail.com>
2026-05-27 11:53:23 +0000

    summary of datatypes

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -1,7 +1,7 @@
 "use strict"; // treats all js codes as newer version
 // alert(3+4) // we are using node js not browser
-console.log(3+3)
-console.log("Anant") //code readability should be high
+//console.log(3+3)
+//console.log("Anant") //code readability should be high
 let age=18
 let name="anant"
 let isLggedIn=true
@@ -15,4 +15,22 @@ undefined=> abhi assign nhi hui h
 symbol => unique
 object => 
 */
-console.log(typeof(null))
+//console.log(typeof(null))
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/*
+types of datatypes: primitive and non primitive
+primitive:7 types: Number, Boolean, String, undefined, null ,BigInt, Symbol
+non-primitive(Reference type):Array, Objects, Functions
+
+*/
+const heroes=["batman", "superman", "ironman"];
+let myObj={
+    name: "Anant",
+    age: 20
+}
+const myFunction = function(){
+     console.log("Hello World"); 
+}
+console.log(typeof(myFunction))
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++

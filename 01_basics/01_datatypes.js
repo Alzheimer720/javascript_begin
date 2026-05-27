88cb4f8bdac9b6745c7f10e018c3ace11644ec89
Alzheimer720 <7anantsharma7@gmail.com>
2026-05-27 09:03:31 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treats all js codes as newer version
+// alert(3+4) // we are using node js not browser
+console.log(3+3)
+console.log("Anant") //code readability should be high
+let age=18
+let name="anant"
+let isLggedIn=true
+/*
+number=> 2 to the power 53
+big int
+string=> ""
+boolean => true / false
+null =>standalone value
+undefined=> abhi assign nhi hui h 
+symbol => unique
+object => 
+*/
+console.log(typeof(null))

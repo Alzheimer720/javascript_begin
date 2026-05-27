2d45b801f54d5e55ae97b4ec5f6ff0ff61ebb1dd
Alzheimer720 <7anantsharma7@gmail.com>
2026-05-27 09:23:07 +0000

    conversion of datatypes

---
+++ 01_basics/01_conversionOperations.js
@@ -0,0 +1,8 @@
+let score="18abc"
+//console.log(typeof (score))
+let ValueInNumber=Number(score)
+//console.log(ValueInNumber)
+//console.log(typeof(ValueInNumber))
+let loggedIn=""
+let booleanLoggedIn=Boolean(loggedIn)
+console.log(booleanLoggedIn)

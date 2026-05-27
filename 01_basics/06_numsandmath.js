840ce9e0e1bee4f756e11df12a2653bd28bbce45
Alzheimer720 <7anantsharma7@gmail.com>
2026-05-27 15:08:26 +0000

    math and num

---
+++ 01_basics/06_numsandmath.js
@@ -0,0 +1,27 @@
+//   +++++++NUMS+++++++++++++++++++
+// const score=400
+// console.log(score)
+// const Balance= new Number(100) // is tareke se balance hamesha ek number hi hoga
+// console.log(Balance)
+// console.log(Balance.toString()) // conversion of number to string
+// console.log(Balance.toString().length) // length of string
+// console.log(Balance.toFixed(3)) // till decimal places
+ 
+// const otherNumber=23.78
+// console.log(otherNumber.toPrecision(3)) //utni digit tk dekho fir roundoff krdo
+// const xNumber=123456
+// console.log(xNumber.toLocaleString()) // isse ham american number system m display kr skte h like hundreds millions etc
+// console.log(xNumber.toLocaleString('en-IN'))
+// ++++++++++++ MATHS +++++++++++++++++++++++
+// console.log(Math.abs(-34))  // mod
+console.log(Math.round(5.5)) // round off
+console.log(Math.ceil(4.1)) // upar wali value
+console.log(Math.floor(4.1)) // niche wali value
+console.log(Math.sqrt(2))
+console.log(Math.random()*10) // random value dega 0 and 1 k beech .. agr question m 1 to 10 kahe 10 se multiply krdo
+console.log(Math.floor(Math.random()*10)+1)
+// agr hame kahe ki 10 se 20 tk beech m random nmbr do then
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+ 
